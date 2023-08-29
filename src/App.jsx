199f5d649da7b0832e0b2e38078ad533533993e5
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-29 09:38:59 +0100

    Create a reusable row styled component and update the styles for the button component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: beige;
   padding: 20px;
 `;
 
@@ -15,10 +15,19 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-
-        <Button onClick={() => alert("Hello, world.")}>Check in</Button>
-        <Button onClick={() => alert("Hello, world.")}>Check out</Button>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Button onClick={() => alert("Hello, world.")}>Check in</Button>
+            <Button
+              variation="secondary"
+              size="small"
+              onClick={() => alert("Hello, world.")}
+            >
+              Check out
+            </Button>
+          </div>
+        </Row>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
