e64063b2c7e6e6d7e865bc8a9a1ef45f98dad7cf
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-29 13:52:03 +0100

    Display cabins data

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      
+
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
