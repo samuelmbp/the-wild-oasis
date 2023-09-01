c898b47082289745c6fe58c00a0a38c5e9ac3d82
Samuel Raducan <samuel.thembp@gmail.com>
2023-09-01 19:35:58 +0100

    Add client-side cabins sorting

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -11,16 +11,24 @@ const CabinTable = () => {
 
   if (isLoading) return <Spinner />;
 
+  // Filtering
   const filterValue = searchParams.get("discount") || "all";
   let filteredCabins;
 
   if (filterValue === "all") filteredCabins = cabins;
-
   if (filterValue === "no-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
-
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // Sorting
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -35,7 +43,7 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
