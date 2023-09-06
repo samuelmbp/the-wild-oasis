4a8c7faf2f91ec6083a37ddd423ca61235de6e3b
Samuel Raducan <samuel.thembp@gmail.com>
2023-09-06 10:44:26 +0100

    Add user authorization

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -11,3 +11,17 @@ export const login = async ({ email, password }) => {
   console.log(data);
   return data;
 };
+
+export const getCurrentUser = async () => {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+};
